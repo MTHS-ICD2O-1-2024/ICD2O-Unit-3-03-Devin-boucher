aa12299e4ceb92f86d73e6a5f3dfa2e5e7f88fac
default <devin.boucher@stu.ocsb.ca>
2025-04-02 16:52:17 +0000

    made website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Devin Boucher
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Calculate Trapezoid
+ */
+function Calculate() {
+  // Get input values
+  const Radius = parseFloat(document.getElementById("Radius").value)
+  const resultDisplay = document.getElementById("Answer")
+
+  // Calculate Volume of Sphere: ( 4 / 3) * Math.PI * Radius ** 3
+  const Volume = (( 4 / 3) * Math.PI * Radius ** 3)
+
+  // Display the result
+  resultDisplay.innerHTML = `The area of the Sphere is: <strong>${Volume.toFixed(2)}</strong> cm²`
+}
